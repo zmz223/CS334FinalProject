4e9c38a09d2e403a6ecc1899bff5a14d51be1ed9
zach <zzaiman@emory.edu>
2022-11-16 18:17:07 -0500

    analysis code

--- zach/trainlib/train.py
+++ zach/trainlib/train.py
@@ -21,6 +21,7 @@ import json
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import argparse
+from classification_models.tfkeras import Classifiers
 
 # Returns configuration in dictionary format.
 def load_config(config_path):
@@ -54,7 +55,7 @@ def generator_wrapper(generator):
 
 
 # Returns image data generators for train, valid, and test splits.
-def get_image_generators(train, valid, test, config):
+def get_image_generators(train, valid, test, config, preprocessing_func):
 
     HEIGHT = int(config['IMG_HEIGHT'])
     WIDTH = int(config['IMG_WIDTH'])
@@ -67,14 +68,14 @@ def get_image_generators(train, valid, test, config):
     print(f"Num labels {str(len(labels))}", flush=True)
 
     train_gen = ImageDataGenerator(
-            # rotation_range=15,
-            # fill_mode='constant',
-            # zoom_range=0.1,
-            # horizontal_flip=True,
-            rescale = 1./255
+            rotation_range=15,
+            fill_mode='constant',
+            zoom_range=0.1,
+            horizontal_flip=True,
+            preprocessing_function=preprocessing_func
     )
 
-    validate_gen = ImageDataGenerator(rescale = 1./255)
+    validate_gen = ImageDataGenerator(preprocessing_function=preprocessing_func)
 
     train_batches = train_gen.flow_from_dataframe(
         train,
@@ -116,19 +117,23 @@ def get_model(config):
 
     num_classes = config['NUM_CLASSES']
     lr = config['INITIAL_LR']
+    model_arch = config['MODEL_ARCHITECTURE']
+    height = config['IMG_HEIGHT']
+    width = config['IMG_WIDTH']
 
     strategy = tf.distribute.MirroredStrategy()
 
     with strategy.scope():
-
-        base_model = DenseNet121(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='max')
+        model_init, preprocessing_func = Classifiers.get(model_arch)
+        base_model = model_init(include_top=False, input_shape=(height,width,3), weights='imagenet', pooling='max')
+        # base_model = DenseNet121(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='max')
         x = base_model.output
-        x = Dense(512, activation = 'relu')(x)
-        x = Dropout(0.3)(x)
-
+        end = Dense(512, activation = 'relu')(x)
+        
         # Add an individual classification layer for every class.
         output = []
         for i in range(num_classes):
+            x = Dropout(0.3)(end)
             output.append(Dense(1, activation='sigmoid')(x))
         
         model = Model(inputs=base_model.input, outputs=output)
@@ -143,7 +148,7 @@ def get_model(config):
             ]
         )
 
-    return model
+    return model, preprocessing_func
 
 def train_model(model, train_ds, valid_ds, config):
 
@@ -151,7 +156,7 @@ def train_model(model, train_ds, valid_ds, config):
     TEST_BATCH = config['TEST_BATCH']
     lr = config['INITIAL_LR']
     epochs = config['MAX_EPOCHS']
-    weights_dir = config['WEIGHTS_DIR']
+    output_path = config['OUTPUT_DIR']
 
     train_epoch = math.ceil(len(train) / BATCH_SIZE)
     val_epoch = math.ceil(len(valid) / BATCH_SIZE)
@@ -160,10 +165,11 @@ def train_model(model, train_ds, valid_ds, config):
                                 patience=2, min_lr=1e-6, verbose=1)
     ES = EarlyStopping(monitor='val_loss', mode='min', patience=4, restore_best_weights=True)
 
+    weights_dir = os.path.join(output_path, 'weights/')
     if not os.path.exists(weights_dir):
         os.makedirs(weights_dir)
     
-    weights_path = os.path.join(weights_dir, 'model.hdf5')
+    weights_path = os.path.join(weights_dir, 'best_model.hdf5')
 
     checkloss = ModelCheckpoint(weights_path, monitor='val_loss', mode='min', verbose=1, save_best_only=True, save_weights_only=False)
 
@@ -183,19 +189,20 @@ if __name__ == '__main__':
     config = load_config('config.json')
     train, valid, test = load_datasets(config)
 
-    train_batches, valid_batches, test_batches = get_image_generators(train, valid, test, config)
+    model, preprocessing_func = get_model(config)
 
-    model = get_model(config)
+    train_batches, valid_batches, test_batches = get_image_generators(train, valid, test, config, preprocessing_func)
 
     history = train_model(model, train_batches, valid_batches, config)
 
     # Predict on test set.
     Y_pred = model.predict(test_batches)
 
+    output_path = config['OUTPUT_DIR']
     # Save pickle files of training history and predictions for further analysis.
-    with open('predictions', 'wb') as f:
+    with open(os.path.join(output_path, 'predictions.pkl'), 'wb') as f:
         pickle.dump(Y_pred, f)
 
-    with open('train_hist', 'wb') as file_pi:
+    with open(os.path.join('train_hist.pkl'), 'wb') as file_pi:
             pickle.dump(history.history, file_pi)
 
